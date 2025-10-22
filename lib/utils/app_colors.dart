3bc67441ced5f4daab627ca8382dd7d704d5d0be
Elboray <elboray456@gmail.com>
2025-10-22 16:29:19 +0300

    refactor: update utils structure and improve code readability

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -5,6 +5,6 @@ class AppColors {
   static const Color primaryColor = Color(0xFFE2BE7F);
   static const Color blackColor = Color(0xFF202020);
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color transparentColor = Colors.transparent;
   static const Color blackBgColor = Color(0x99202020);
+  static const Color transparentColor = Colors.transparent;
 }
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+class AppStyles {
+  static final TextStyle bold16white = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: AppColors.whiteColor,
+  );
+  static final TextStyle bold20white = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: AppColors.whiteColor,
+  );
+  static final TextStyle bold36white = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 36,
+    color: AppColors.whiteColor,
+  );
+  static final TextStyle bold30white = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    color: AppColors.whiteColor,
+  );
+  static final TextStyle bold20primary = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: AppColors.primaryColor,
+  );
+  static final TextStyle bold24primary = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+    color: AppColors.primaryColor,
+  );
+  static final TextStyle bold14white = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+    color: AppColors.whiteColor,
+  );
+  static final TextStyle bold24black = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+    color: AppColors.blackColor,
+  );
+  static final TextStyle bold16black = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: AppColors.blackColor,
+  );
+  static final TextStyle bold20black = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: AppColors.blackColor,
+  );
+  static final TextStyle bold14black = GoogleFonts.elMessiri(
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+    color: AppColors.blackColor,
+  );
+}
