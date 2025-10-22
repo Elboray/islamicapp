7d7671e93a63c3626c1aee742fa66ec000c9c384
Elboray <elboray456@gmail.com>
2025-10-22 16:34:04 +0300

    feat(feature/assets): add all application images and organize asset structure

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,25 @@
+class AppAssets {
+  static const String quranBg = 'assets/images/Background.png';
+  static const String hadethBg = 'assets/images/background_hadeth.png';
+  static const String sebhaBg = 'assets/images/background_sebha.png';
+  static const String radioBg = 'assets/images/background_radio.png';
+  static const String timeBg = 'assets/images/background_time.png';
+  static const String iconQuran = 'assets/images/icon-quran.png';
+  static const String iconHadeth = 'assets/images/icon_hadeth.png';
+  static const String iconRadio = 'assets/images/icon_radio.png';
+  static const String iconSebha = 'assets/images/icon_sebha.png';
+  static const String iconTime = 'assets/images/icon_time.png';
+  static const String logo = 'assets/images/islami_background_image.png';
+  static const String iconSearch = 'assets/images/icon_search.png';
+  static const String mostRecently = 'assets/images/img_most_recent.png';
+  static const String starVector = 'assets/images/star.png';
+  static const String bgSuraDetails = 'assets/images/bg_sura_details.png';
+  static const String hadethBgCard = 'assets/images/hadeth_bg_card.png';
+  static const String imgLeftCorner = 'assets/images/img_left_corner.png';
+  static const String imgRightCorner = 'assets/images/img_right_corner.png';
+  static const String mosqueHadeth = 'assets/images/mosque_hadeth.png';
+  static const String sebha = 'assets/images/sebha.png';
+  static const String audioBg = 'assets/images/audio_bg.png';
+  static const String azkarHelalBg = 'assets/images/helal_bg.png';
+  static const String prayTimeBg = 'assets/images/pray_time.png';
+}
